8708337a4063284c2ec26239a54a07b3acf4ea1f
tharwatsamy <tharwatsamy2004@gmail.com>
2022-09-28 11:17:04 +0200

    init hive and create todos view

---
+++ lib/views/todos_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TodosView extends StatelessWidget {
+  const TodosView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
